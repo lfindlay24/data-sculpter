06de9f86e238e1add73416e43c9c23d7711c5109
Nick-Paxton <npaxton@student.neumont.edu>
2024-08-01 11:13:36 -0600

    Fixed gitignore and updated authorizer and update user

--- sam-data-sculptor/user_api/update.py
+++ sam-data-sculptor/user_api/update.py
@@ -3,6 +3,7 @@ from boto3.dynamodb.conditions import Key
 from os import getenv
 import json
 import bcrypt
+from base64 import b64encode
 
 region_name = getenv('APP_REGION')
 users_table = boto3.resource('dynamodb', region_name=region_name).Table('Users')
@@ -20,14 +21,36 @@ def lambda_handler(event, context):
     
     user_id = path["userId"]
     user = users_table.get_item(Key={"user_id": user_id})
-    if user is None:
+    print(user)
+    if "Item" not in None:
         return response(404, "user not found")
+    
+    user = user["Item"]
+
+    if not check_password(body["old_password"], user["password"]):
+        return response(400, "incorrect old password")
+    
+    if "email" in body:
+        user["email"] = body["email"]
+    if "new_password" in body:
+        user["password"] = salt_hash_password(body["new_password"])
+    
+    users_table.put_item(Item=user)
+
+    stringtoencode = f"{body["email"]}:{body['new_password']}"
+    encoded = b64encode(stringtoencode.encode('utf-8'))
+    print(f"Encoded: {encoded}")
+
+    return response(200, {"Authorization": encoded.decode("utf-8")})
 
 def salt_hash_password(password):
     encoded = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(encoded, salt)
     return hash
+
+def check_password(password, hashed_password):
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 def response(code, body):
 
